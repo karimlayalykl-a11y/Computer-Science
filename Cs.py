793ee69efdaf4b8a7d77044fbe0f58f00dafe953
karimlayalykl-a11y <karim.layaly.kl@gmail.com>
2025-12-10 18:34:50 +0200

    sixth commit

--- Cs.py
+++ Cs.py
@@ -56,63 +56,63 @@ PC2 = [14,17,11,24,1,5,
     44,49,39,56,34,53,
     46,42,50,36,29,32]
 
-SBOX = # S1
-[
+SBOX = [ #S1
+    [
         [14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7],
         [0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8],
         [4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0],
         [15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13]
     ],
     # S2
-[
+    [
         [15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10],
         [3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5],
         [0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15],
         [13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9]
     ],
     # S3
-[
+    [
         [10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8],
         [13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1],
         [13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7],
         [1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12]
     ],
     # S4
-[
+    [
         [7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15],
         [13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9],
         [10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4],
         [3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14]
     ],
     # S5
-[
+    [
         [2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9],
         [14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6],
         [4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14],
         [11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3]
     ],
     # S6
-[
+    [
         [12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11],
         [10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8],
         [9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6],
         [4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13]
     ],
     # S7
-[
+    [
         [4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1],
         [13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6],
         [1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2],
         [6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12]
     ],
     # S8
-[
+    [
         [13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7],
         [1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2],
         [7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8],
         [2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11]
     ]
-
+]
 
 #Functions 
 
@@ -198,6 +198,27 @@ def feistel(R:str, roundkey:str):
 
 
 
+        #Encryption ////// Decryption 
+
+def encrypt(block64:str) -> str:
+    #encrypting a single 64 bit block using DES
+    permuted = permutation(block64, IP)
+    R, L = permuted[:32], permuted[32:]  #IP
+
+    for i in range(16):
+        R, L = R, XORing(L, feistel(R, subkeys[i]))
+    combined = R + L 
+    return permutation(combined,FP)
+
+def decryption(block64, IP):
+    #decrypting a single 64 bit block using DES
+    #same process but subkeys applied in reverse order
+    permuted = permutation(block64, IP)
+    R, L = permuted[32:], permuted[:32]
+    for i in range(16):
+        R, L = R, XORing(L, feistel(R, subkeys[15 - i]))
+    combined = R + L
+    return permutation(combined, FP)
 
 
 
@@ -207,13 +228,20 @@ def feistel(R:str, roundkey:str):
 
 
 
+#------------------------------------------------------------------------------------------------------------------
 
-text_input = input("Enter plaintext:")
-block = text_input[8:]
-while len (block) <8:
-    block += ' ' 
+            #Main 
+
+#------------------------------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+
+    text_input = input("Enter plaintext:")
+    block = text_input[:8]
+    while len (block) <8:
+        block += ' ' 
     #padding with spaces if less than 8 characters
-print ("\n [NOTE] Using the first 8 characters only:", block)
+    print ("\n [NOTE] Using the first 8 characters only:", block)
     #making sure the user knows that we need only the first 8 characters
 
 
@@ -239,4 +267,42 @@ key_in_binary= bin(int(key_in_hex, 16))[2:]
 while len(key_in_binary) < 64:
     key_in_binary = '0' + key_in_binary
 
-subkeys= subkeys_generated(key_in_binary)
+subkeyss= subkeys(key_in_binary)
+
+
+
+#Run mode main
+
+
+mode = print(input)("Please select mode: Encrypt (E) or Decrypt(D):") .strip().upper()
+
+
+if mode == "E": 
+    cipher_binary = encrypt(binary_text)
+    cipher_hexa = hex(int(cipher_binary, 2))[2:].upper().zfill(16) 
+
+    print("\n ENCRYPTION ")
+    print("Cipher (Hexadecimal):", cipher_hexa)
+    print("Cipher (binary):", cipher_binary)
+
+elif mode == "D":
+    cipher_input = input("Please Enter ciphertext (16-character HEX)").strip()
+    cipher_binary = bin(int(cipher_input,16))[2:].zfill(16)
+    plain_binary = decryption(cipher_binary)
+    plain_hexa = hex(int(plain_binary, 2))[2:].upper()
+    plain_text = bytes.fromhex(plain_hexa).decode('ascii', 'ignore').rstrip()    #convert decrypted hex string to ASCII
+
+    #telling the user to enter the ciphertext 
+    #converting from hexa to binary
+    #decrypt binary
+    #convert from binary to hexa
+    #convert hexa to ASCII
+
+    print("\n DECRYPTION ")
+    print("Plaintext (Hexadecimal):", plain_hexa)
+    print("Plaintext (Binary):", plain_binary)
+    print("ASCII Final Output :", plain_text)
+
+
+else: 
+    print ("ERROR:invalid, Please choose 'E' for Encryption or 'D' for Decryption")
